6acddfd7e5d37852d2e31b492bce3bab66b67cb1
AshrafUAsif <108550504+AshrafUAsif@users.noreply.github.com>
2023-04-01 01:30:27 +0600

    header section added

---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Blog.css'
+const Blog = () => {
+    return (
+        <div className='blog-container'>
+            <div className="blogs-container">
+
+            </div>
+            <div className="bookmarked-container">
+                
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/profile2.png';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <h2>Knowledge Cafe</h2>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
